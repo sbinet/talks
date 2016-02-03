4e16a94be83766ea6190ed52658c9c9202b518b7
Sebastien Binet <binet@cern.ch>
2016-02-03 23:18:00 +0100

    20160204-hep-strikes-back: make hello progs executable

---
+++ 2016/20160204-hep-strikes-back/_code/hello.cxx.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	src := `
+// START OMIT
+#include <iostream>
+int main(int, char **) {
+  std::cout << "Hello from C++" << std::endl;
+  return 0;
+}
+// END OMIT
+`
+
+	dir, err := ioutil.TempDir("", "go-compile-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	err = os.Chdir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("hello.cxx", []byte(src), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = exec.Command("c++", "-o", "hello", "hello.cxx").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("./hello")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ 2016/20160204-hep-strikes-back/_code/hello.f.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	fortran := `
+c     START OMIT
+c     == hello.f ==
+      program main
+      implicit none
+      write ( *, '(a)' ) 'Hello from FORTRAN'
+      stop
+      end
+c     END OMIT
+`
+
+	dir, err := ioutil.TempDir("", "go-compile-fortran-")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	err = os.Chdir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("hello.f", []byte(fortran), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = exec.Command("gfortran", "-c", "hello.f").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = exec.Command("gfortran", "-o", "hello", "hello.o").Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command("./hello")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
